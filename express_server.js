e3b988404dd6e2c0b89f2f673765fa7c10ac83dc
Josue <arevalo.josued@gmail.com>
2021-12-07 02:54:36 +0000

    adding in generate random string function

--- express_server.js
+++ express_server.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  let characters = "abcdefghijklmnopqrstuvwxyz1234567890";
+  let random = "";
+  for (let i = 0; i < 6; i++)  {
+    random += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return random;
+}
+
+
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
